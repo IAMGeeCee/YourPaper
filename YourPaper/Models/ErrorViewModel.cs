aab3866e7e43ef3f5bca778084cb6dcbffa9a8eb
IAMGeeCee <george.clensy@gmail.com>
2023-01-06 17:29:58 +0000

    Project created

---
+++ YourPaper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YourPaper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
