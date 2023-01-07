791ea0b620be0f1a2054174f7f2b5c63e842fa5b
IAMGeeCee <george.clensy@gmail.com>
2023-01-07 09:07:21 +0000

    Created YourPaper Desktop and Front end work

---
+++ YourPaper Desktop/Upload.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace YourPaper_Desktop
+{
+    partial class Upload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.pnlForm = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.imgImage = new System.Windows.Forms.PictureBox();
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.ofdFileSelect = new System.Windows.Forms.OpenFileDialog();
+            this.pnlForm.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTitle.Location = new System.Drawing.Point(116, 107);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(302, 20);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // pnlForm
+            // 
+            this.pnlForm.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlForm.BackColor = System.Drawing.Color.White;
+            this.pnlForm.Controls.Add(this.btnUpload);
+            this.pnlForm.Controls.Add(this.btnSelectImage);
+            this.pnlForm.Controls.Add(this.imgImage);
+            this.pnlForm.Controls.Add(this.label3);
+            this.pnlForm.Controls.Add(this.txtDescription);
+            this.pnlForm.Controls.Add(this.label2);
+            this.pnlForm.Controls.Add(this.label1);
+            this.pnlForm.Controls.Add(this.txtTitle);
+            this.pnlForm.Location = new System.Drawing.Point(181, 21);
+            this.pnlForm.Margin = new System.Windows.Forms.Padding(30);
+            this.pnlForm.Name = "pnlForm";
+            this.pnlForm.Size = new System.Drawing.Size(538, 488);
+            this.pnlForm.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI Semilight", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(175, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Upload a wallpaper";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI Semilight", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(239, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Title:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI Semilight", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(208, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Description:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDescription.Location = new System.Drawing.Point(116, 172);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(302, 20);
+            this.txtDescription.TabIndex = 3;
+            // 
+            // imgImage
+            // 
+            this.imgImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imgImage.Location = new System.Drawing.Point(116, 276);
+            this.imgImage.Name = "imgImage";
+            this.imgImage.Size = new System.Drawing.Size(302, 112);
+            this.imgImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgImage.TabIndex = 5;
+            this.imgImage.TabStop = false;
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectImage.Location = new System.Drawing.Point(116, 247);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(302, 23);
+            this.btnSelectImage.TabIndex = 6;
+            this.btnSelectImage.Text = "Select Image";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpload.Location = new System.Drawing.Point(116, 452);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(302, 23);
+            this.btnUpload.TabIndex = 7;
+            this.btnUpload.Text = "Upload!";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            // 
+            // ofdFileSelect
+            // 
+            this.ofdFileSelect.FileName = "openFileDialog1";
+            this.ofdFileSelect.Title = "Select Image";
+            // 
+            // Upload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 548);
+            this.Controls.Add(this.pnlForm);
+            this.Name = "Upload";
+            this.Text = "Upload";
+            this.pnlForm.ResumeLayout(false);
+            this.pnlForm.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Panel pnlForm;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.PictureBox imgImage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog ofdFileSelect;
+    }
+}
---
+++ YourPaper Desktop/Upload.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace YourPaper_Desktop
+{
+    public partial class Upload : Form
+    {
+        public Upload()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectImage_Click(object sender, EventArgs e)
+        {
+
+            ofdFileSelect.Multiselect = false;
+            if (ofdFileSelect.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = Path.GetFileName(ofdFileSelect.FileName);
+                string filePath = ofdFileSelect.FileName;
+           
+                imgImage.Image = Image.FromStream(ofdFileSelect.OpenFile());
+            }
+        }
+    }
+}
